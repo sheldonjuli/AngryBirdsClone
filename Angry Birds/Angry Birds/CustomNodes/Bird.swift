01bf5ec41ce4f296806ad4cf2c5aa0da3303faf9
Li Ju <sheldon.juli@gmail.com>
2018-09-15 14:12:30 -0400

    Added bird class

---
+++ Angry Birds/Angry Birds/CustomNodes/Bird.swift
@@ -0,0 +1,38 @@
+//
+//  Bird.swift
+//  Angry Birds
+//
+//  Created by Li Ju on 2018-09-15.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import SpriteKit
+
+enum BirdType: String {
+    case red, blue, yellow, gray
+}
+
+class Bird: SKSpriteNode {
+    let birdType: BirdType
+    
+    init(type: BirdType) {
+        birdType = type
+        
+        let color: UIColor!
+        switch type {
+        case .red:
+            color = UIColor.red
+        case .blue:
+            color = UIColor.blue
+        case .yellow:
+            color = UIColor.yellow
+        case .gray:
+            color = UIColor.lightGray
+        }
+        super.init(texture: nil, color: color, size: CGSize(width: 40.0, height: 40.0))
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- Angry Birds/Angry Birds/Scenes/GameScene.swift
+++ Angry Birds/Angry Birds/Scenes/GameScene.swift
@@ -17,6 +17,9 @@ class GameScene: SKScene {
     var pinchRecognizer = UIPinchGestureRecognizer()
     var maxScale: CGFloat = 0
     
+    var bird = Bird(type: .red)
+    let anchor = SKNode()
+    
     override func didMove(to view: SKView) {
         setupLevel()
         setupGestureRecognizers()
@@ -38,6 +41,9 @@ class GameScene: SKScene {
         }
         
         addCamera()
+        anchor.position = CGPoint(x: mapNode.frame.midX / 2, y: mapNode.frame.midY / 2)
+        addChild(anchor)
+        addBird()
     }
     
     func addCamera() {
@@ -46,6 +52,11 @@ class GameScene: SKScene {
         gameCamera.position = CGPoint(x: view.bounds.size.width / 2, y: view.bounds.size.height / 2)
         camera = gameCamera
         gameCamera.setContraints(with: self, and: mapNode.frame, to: nil)
+    }
+    
+    func addBird() {
+        bird.position = anchor.position
+        addChild(bird)
     }
 }
 
