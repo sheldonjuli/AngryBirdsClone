1e123eb7430dec166705708afa76b21d6ad12939
Li Ju <sheldon.juli@gmail.com>
2018-09-14 21:36:48 -0400

    Added camera

--- Angry Birds/Angry Birds/Scenes/GameScene.swift
+++ Angry Birds/Angry Birds/Scenes/GameScene.swift
@@ -11,9 +11,27 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let gameCamera = SKCameraNode()
+    
+    var mapNode = SKTileMapNode()
+    let gameCamera = GameCamera()
+    var panRecognizer = UIPanGestureRecognizer()
     
     override func didMove(to view: SKView) {
+        setupLevel()
+        setupGestureRecognizers()
+    }
+    
+    func setupGestureRecognizers() {
+        guard let view = view else { return }
+        panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(pan))
+        view.addGestureRecognizer(panRecognizer)
+    }
+    
+    func setupLevel() {
+        if let mapNode = childNode(withName: "Tile Map Node") as? SKTileMapNode {
+            self.mapNode = mapNode
+        }
+        
         addCamera()
     }
     
@@ -22,5 +40,15 @@ class GameScene: SKScene {
         addChild(gameCamera)
         gameCamera.position = CGPoint(x: view.bounds.size.width / 2, y: view.bounds.size.height / 2)
         camera = gameCamera
+        gameCamera.setContraints(with: self, and: mapNode.frame, to: nil)
+    }
+}
+
+extension GameScene {
+    @objc func pan(sender: UIPanGestureRecognizer) {
+        guard let view = view else { return }
+        let translation = sender.translation(in: view)
+        gameCamera.position = CGPoint(x: gameCamera.position.x - translation.x, y: gameCamera.position.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: view)
     }
 }
