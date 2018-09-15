fe0b97b594a61f386f01d59bd353c2e37564344d
Li Ju <sheldon.juli@gmail.com>
2018-09-15 13:11:20 -0400

    Added pinch to zoom

---
+++ Angry Birds/Angry Birds/Template/Configuration.swift
@@ -0,0 +1,15 @@
+//
+//  Configuration.swift
+//  Angry Birds
+//
+//  Created by Li Ju on 2018-09-15.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGPoint {
+    static public func * (left: CGPoint, right: CGFloat) -> CGPoint {
+        return CGPoint(x: left.x * right, y: left.y * right)
+    }
+}
