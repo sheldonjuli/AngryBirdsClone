16dc30b1573e13e7ee40a3e0db2951f874272944
Li Ju <sheldon.juli@gmail.com>
2018-09-19 13:25:13 -0400

    Added level selections

--- Angry Birds/Angry Birds/Scenes/LevelScene.swift
+++ Angry Birds/Angry Birds/Scenes/LevelScene.swift
@@ -10,4 +10,38 @@ import SpriteKit
 
 class LevelScene: SKScene {
     var sceneManagerDelegate: SceneManagerDelegate?
+    
+    override func didMove(to view: SKView) {
+        setupLevelSelection()
+    }
+    
+    func setupLevelSelection() {
+        var level = 1
+        let columnStartingPoint = frame.midX / 2
+        let rowStartingPoint = frame.midY + frame.midY / 2
+        for row in 0..<3 {
+            for column in 0..<3 {
+                let levelBoxButton = SpriteKitButton(defaultButtonImage: "woodButton", action: goToGameSceneFor, index: level)
+                levelBoxButton.position = CGPoint(x: columnStartingPoint + CGFloat(column) * columnStartingPoint, y: rowStartingPoint - CGFloat(row) * frame.midY / 2)
+                
+                levelBoxButton.zPosition = ZPositions.hudBackground
+                addChild(levelBoxButton)
+                
+                let levelLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+                levelLabel.fontSize = 200.0
+                levelLabel.verticalAlignmentMode = .center
+                levelLabel.text = "\(level)"
+                levelLabel.aspecScale(to: levelBoxButton.size, width: false, multiplier: 0.35)
+                levelLabel.zPosition = ZPositions.hudLabel
+                
+                levelBoxButton.addChild(levelLabel)
+                levelBoxButton.aspecScale(to: frame.size, width: false, multiplier: 0.2)
+                level += 1
+            }
+        }
+    }
+    
+    func goToGameSceneFor(level: Int) {
+        sceneManagerDelegate?.presentGameSceneFor(level: level)
+    }
 }
