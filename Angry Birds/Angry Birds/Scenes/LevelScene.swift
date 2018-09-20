543147abe194e902de381a39cf3c81df873ca47e
Li Ju <sheldon.juli@gmail.com>
2018-09-20 17:53:36 -0400

    Added menu and level background

--- Angry Birds/Angry Birds/Scenes/LevelScene.swift
+++ Angry Birds/Angry Birds/Scenes/LevelScene.swift
@@ -16,6 +16,12 @@ class LevelScene: SKScene {
     }
     
     func setupLevelSelection() {
+        let background = SKSpriteNode(imageNamed: "levelBackground")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.aspecScale(to: frame.size, width: true, multiplier: 1.0)
+        background.zPosition = ZPositions.background
+        addChild(background)
+
         var level = 1
         let columnStartingPoint = frame.midX / 2
         let rowStartingPoint = frame.midY + frame.midY / 2
--- Angry Birds/Angry Birds/Scenes/MenuScene.swift
+++ Angry Birds/Angry Birds/Scenes/MenuScene.swift
@@ -16,9 +16,17 @@ class MenuScene: SKScene {
     }
     
     func setupMenu() {
+        
+        let background = SKSpriteNode(imageNamed: "menuBackground")
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        background.aspecScale(to: frame.size, width: true, multiplier: 1.0)
+        background.zPosition = ZPositions.background
+        addChild(background)
+        
         let button = SpriteKitButton(defaultButtonImage: "playButton", action: goToLevelScene, index: 0)
-        button.position = CGPoint(x: frame.midX, y: frame.midY)
+        button.position = CGPoint(x: frame.midX, y: frame.midY * 0.8)
         button.aspecScale(to: frame.size, width: false, multiplier: 0.2)
+        button.zPosition = ZPositions.hudLabel
         addChild(button)
     }
     
