5f73a4238eef0cd97d8aa75cff0ac9c823b2aace
Li Ju <sheldon.juli@gmail.com>
2018-09-20 20:41:33 -0400

    Added enemy class

---
+++ Angry Birds/Angry Birds/CustomNodes/Enemy.swift
@@ -0,0 +1,13 @@
+//
+//  Enemy.swift
+//  Angry Birds
+//
+//  Created by Li Ju on 2018-09-20.
+//  Copyright © 2018 Li Ju. All rights reserved.
+//
+
+import UIKit
+
+class Enemy: SKSpriteNode {
+
+}
